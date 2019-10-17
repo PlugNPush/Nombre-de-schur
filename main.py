07eeb3441e6e74d2a89a908810631980f932f313
PlugNCS <michael.nass@free.fr>
2019-10-17 19:30:31 +0200

    Element et triplet

--- main.py
+++ main.py
@@ -6,7 +6,38 @@ class Color:
         self.orange = '\033[33m'
         self.blue = '\033[34m'
         self.purple = '\033[31m'
+        
+class Element:
+    def __init__(self, a, b):
+        self.value = a
+        self.color = b
+        
+class Triplet:
+    def __init__(self, a, b, c):
+        self.element = a
+        self.element2 = b
+        self.element3 = c
+    
+    def isSameColor(self):
+        if self.element.color == self.element2.color and self.element.color == self.element3.color:
+            return True
+        else:
+            return False
+        
 
 color = Color()
+element = Element(2, color.green)
+element2 = Element(4, color.red)
+element3 = Element(1, color.purple)
 
-print(color.purple, "Bonjour !")
+triplet = Triplet(element, element2, element3)
+
+print(triplet.element.value, "TEST")
+
+if triplet.isSameColor():
+    print("Same color")
+else:
+    print("Not same color")
+
+
+
