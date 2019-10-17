a94e6a0ffa564bfaacd066247c44c65502cff248
PlugNCS <michael.nass@free.fr>
2019-10-17 18:35:37 +0200

    test

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print("OK!")
-print("yes")
-print("ça marche")
-print("évidemment")
+class Color:
+    def __init__(self):
+        self.white = '\033[0m'
+        self.red = '\033[31m'
+        self.green = '\033[32m'
+        self.orange = '\033[33m'
+        self.blue = '\033[34m'
+        self.purple = '\033[31m'
+
+color = Color()
+
+print(color.purple, "Bonjour !")
