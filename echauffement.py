5ecaab22a9cc38e5256b570556b91d556a15f0da
PlugNCS <michael.nass@free.fr>
2019-10-29 10:56:43 +0100

    Echauffement

---
+++ echauffement.py
@@ -0,0 +1,30 @@
+class Color:
+    def __init__(self):
+        self.white = '\033[0m'
+        self.red = '\033[31m'
+        self.green = '\033[32m'
+        self.orange = '\033[33m'
+        self.blue = '\033[34m'
+        self.purple = '\033[31m'
+        
+class Element:
+    def __init__(self, a, b):
+        self.value = a
+        self.color = b
+        
+class Triplet:
+    def __init__(self, a, b, c):
+        self.element = a
+        self.element2 = b
+        self.element3 = c
+    
+    def isSameColor(self):
+        if self.element.color == self.element2.color and self.element.color == self.element3.color:
+            return True
+        else:
+            return False
+        
+def exo1:
+    let colors = Color()
+    
+exo1()
