18bc2ed3775b59744b0a97f0afbb0b93084099c0
PlugNCS <michael.nass@free.fr>
2019-11-03 15:43:17 +0100

    MODIFS

--- echauffement.py
+++ echauffement.py
@@ -1,3 +1,5 @@
+import random
+
 class Color:
     def __init__(self):
         self.white = '\033[0m'
@@ -5,8 +7,16 @@ class Color:
         self.green = '\033[32m'
         self.orange = '\033[33m'
         self.blue = '\033[34m'
-        self.purple = '\033[31m'
-        
+        self.purple = '\033[35m'
+        self.colors = [""] * 6
+        self.colors[0] = self.white
+        self.colors[1] = self.red
+        self.colors[2] = self.green
+        self.colors[3] = self.orange
+        self.colors[4] = self.blue
+        self.colors[5] = self.purple
+                
+
 class Element:
     def __init__(self, a, b):
         self.value = a
@@ -27,10 +37,20 @@ class Triplet:
 def exo1():
     colors = Color()
     n = int(input("Donnez n: "))
-    for i in range(0, n):
+    for i in range(1, n+1):
         if i%2 == 0:
             print(colors.red, i)
         else:
             print(colors.blue, i)
+
+def exo2():
+    colors = Color()
+    x = int(input("Combien de nombres: "))
+    n = int(input("Combien de couleurs ? (n <= 6): "))
+    colorselection = [""] * 6
+    colorselection = random.sample(colors.colors, n)
+    for i in range(1, x+1):
+        print(random.choice(colorselection), i)
     
-exo1()
+    
+exo2()
